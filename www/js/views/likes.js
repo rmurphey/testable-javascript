178b58dc2593ddea4261400df3ac47c937f53af4
Rebecca Murphey <rmurphey@gmail.com>
2012-11-11 14:47:19 +0000

    remove accidental stuff

--- www/js/views/likes.js
+++ www/js/views/likes.js
@@ -1,6 +1,6 @@
 var app = window.app = window.app || {};
 
-app.Views.Likes = (function() {
+app.Likes = (function() {
   var Likes = function( settings ) {
     this.$el = $( settings.el );
   };
@@ -11,59 +11,4 @@ app.Views.Likes = (function() {
   };
 
   return Likes;
-}());
-
-var app = window.app = window.app || {};
-
-app.Search = (function() {
-  var Search = function() {};
-
-  var processResults = function( resp ) {
-    return resp.results;
-  };
-
-  Search.prototype.fetch = function( query ) {
-    return $.getJSON( '/data/search.json', {
-      q : query
-    }).pipe( processResults );
-  };
-
-  return Search;
-}());
-
-var app = window.app = window.app || {};
-
-app.Views.SearchResults = (function() {
-
-  var SearchResults = function( settings ) {
-    this.app = settings.app;
-    this.$el = $( settings.el );
-
-    this.$el.on( 'click', '.like', $.proxy( this, '_handleLike' ) );
-
-    RSVP.EventTarget.mixin(this);
-  };
-
-  SearchResults.prototype.set = function( people ) {
-    var $el = this.$el;
-    $el.empty();
-
-    return app.loadTemplate( 'people-detailed.tmpl' ).done(function( t ) {
-      var html = t( { people : people } );
-      $el.html( html );
-    });
-  };
-
-  SearchResults.prototype._handleLike = function( evt ) {
-    evt.preventDefault();
-    var name = this._getName( evt.currentTarget );
-    this.app.add( 'liked', name );
-  };
-
-  SearchResults.prototype._getName = function( el ) {
-    return $( el ).closest( 'li' ).attr( 'data-name' );
-  };
-
-  return SearchResults;
-
 }());
